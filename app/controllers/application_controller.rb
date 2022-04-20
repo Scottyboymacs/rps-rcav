31343687f7178a53e34a10b1f533d2c3880e8e4c
Scott McKeon <scott.mckeon@chicagobooth.edu>
2022-04-20 21:46:48 +0000

    Rock paper and scissors complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,40 @@ class ApplicationController < ActionController::Base
   end
 
   def play_rock
-    #render something
-    render({ :template => "game_templates/play_rock".html_safe })
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @output = "tied"
+    elsif  @comp_move == "scissors"
+      @output = "won"
+    elsif  @comp_move == "paper"
+      @output = "lost"
+    end    
+      render({ :template => "game_templates/play_rock".html_safe })
   end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @output = "lost"
+    elsif  @comp_move == "scissors"
+      @output = "tied"
+    elsif  @comp_move == "paper"
+      @output = "won"
+    end    
+      render({ :template => "game_templates/play_scissors".html_safe })
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @output = "won"
+    elsif  @comp_move == "scissors"
+      @output = "lost"
+    elsif  @comp_move == "paper"
+      @output = "tied"
+    end    
+      render({ :template => "game_templates/play_paper".html_safe })
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   self.get("/rock", { :controller => "application", :action => "play_rock"})
+  self.get("/paper", { :controller => "application", :action => "play_paper"})
+  self.get("/scissors", { :controller => "application", :action => "play_scissors"})
 
   self.get("/", { :controller => "application", :action => "homepage"})
 
